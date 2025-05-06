8b19fa312e193c9b7f17442124e80cc5c9dfa8b6
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-05-06 13:22:35 -0300

    Project created

---
+++ src/main/java/tech/backend/SeuImovel/SeuImovelApplication.java
@@ -0,0 +1,13 @@
+package tech.backend.SeuImovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuImovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeuImovelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/backend/SeuImovel/SeuImovelApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.backend.SeuImovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeuImovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
