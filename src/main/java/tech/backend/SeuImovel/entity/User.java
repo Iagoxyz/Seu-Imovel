22cf5c6034a6fe8b6a960c215ef23bade0984fef
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-05-08 10:46:10 -0300

    User entity

---
+++ src/main/java/tech/backend/SeuImovel/entity/User.java
@@ -0,0 +1,24 @@
+package tech.backend.SeuImovel.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+}
